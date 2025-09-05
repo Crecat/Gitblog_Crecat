e90d9abb57ddbec726d7ed5ecb8f9751a79b50d8
Crecat_bjkim <bjkim@cre.cat>
2025-09-05 16:12:59 +0900

    🚧 Fix-blog-system: change commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        '➕ Add-post',
+        '➖ Remove-post',
+        '🔨 Update-post',
+        '⬆️ Upgrade-dependency',
+        '🚧 Fix-blog-system',
+        '🔧 Upgrade-blog-system'
+      ]
+    ]
+  }
+};
